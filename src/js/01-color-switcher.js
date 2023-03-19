bd8311be9b895c11de53bf0bc69e8f7f9ed71ebf
artemkravchenk0 <kravchenkoartem@ukr.net>
2023-03-19 18:21:33 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+ function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  let intervalId;
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  const body = document.querySelector('body');
+
+  startBtn.addEventListener('click', startColorChanging);
+  stopBtn.addEventListener('click', stopColorChanging);
+
+  function startColorChanging() {
+    intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+  }
+
+  function stopColorChanging() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+  }
 
